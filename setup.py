cde88594d172f3d06e1af02eb34d9dfd670788e6
Adam Chainz <adam@adamj.eu>
2016-01-15 16:11:50 +0000

    setup.py add 'publish'

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+import sys
 
 from setuptools import find_packages, setup
 
@@ -16,6 +17,22 @@ def get_version(package):
 
 
 version = get_version('yplan_logging_utils')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("rm -rf .eggs/ build/ dist/")
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a v%s -m 'Version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
 
 
 with open('README.rst') as readme_file:
